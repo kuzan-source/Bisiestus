403685be677e18e40b211fb1ce093a76334e62fb
kuzan-source <h39krot@gmail.com>
2025-12-19 18:03:02 -0600

    fix: Corregido el package

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.espiralsoft.bisiestus.ui.theme
+package com.espiralsoft.bisiestus.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
