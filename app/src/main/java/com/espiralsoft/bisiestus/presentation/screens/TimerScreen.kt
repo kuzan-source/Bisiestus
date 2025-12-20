969dfe6d4d0f9d7f09e5599ca017c739ef8c4fd7
kuzan-source <h39krot@gmail.com>
2025-12-19 21:57:54 -0600

    del: LaunchedEffect in TimerScreen

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/screens/TimerScreen.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/screens/TimerScreen.kt
@@ -4,12 +4,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.NonCancellable.key
 import com.espiralsoft.bisiestus.presentation.viewModel.TimerViewModel
 
 @Composable
@@ -18,10 +16,6 @@ fun TimerScreen(
 ){
     // Obtenemos el estado del ViewModel
     val state by viewModel.state.collectAsState()
-
-    LaunchedEffect(key) {
-        viewModel.showCurrentTime()
-    }
 
     // El Box actuará como el contenedor principal de la pantalla
     Box(
