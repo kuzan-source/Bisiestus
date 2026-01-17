15adf36af0e4c2c8b7592c12557628bffe549a89
kuzan-source <h39krot@gmail.com>
2026-01-16 23:35:56 -0600

    update: eliminación de FontFamily.Monospace

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/components/TimeSeparator.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/components/TimeSeparator.kt
@@ -5,14 +5,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TimeSeparator() {
     Text(
         text = ":",
-        fontFamily = FontFamily.Monospace,
         style = MaterialTheme.typography.displayMedium,
         modifier = Modifier.padding(horizontal = 4.dp)
     )
