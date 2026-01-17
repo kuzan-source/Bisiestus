d98230566f6dbb58402be20a2084f0fd26a498a1
kuzan-source <h39krot@gmail.com>
2026-01-16 22:55:09 -0600

    mod: eliminado comentarios y nuevos comentarios en Theme.kt

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Theme.kt
@@ -20,25 +20,18 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun BisiestusTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+
+    // Esquema de colores basado en el wallpaper
+    // y en el modo del dispositivo(Modo oscuro o Modo claro)
+    // pd: Este codigo fue autogenerado, asi que lo dejo nomas porque si
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
@@ -49,9 +42,11 @@ fun BisiestusTheme(
         else -> LightColorScheme
     }
 
+    //Envoltura que contiene las variables esteticas
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
+
 }
