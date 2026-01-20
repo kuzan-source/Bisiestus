f9f66be999542a0fa1b7ae11af844d8285682ec1
kuzan-source <h39krot@gmail.com>
2026-01-20 14:23:07 -0600

    add : Estilo titleLarge a las Typography

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Type.kt
@@ -7,6 +7,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
+
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Monospace,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
+    ),
+
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
