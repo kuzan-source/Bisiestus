ffddbcd3305e2ba86719e7df34bd66581a6156cc
kuzan-source <h39krot@gmail.com>
2026-01-19 18:09:06 -0600

    update: reestructuración de bloques buildFeatures y kotlin en build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        }
-    }
+
     buildFeatures {
         compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
 
