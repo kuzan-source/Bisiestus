d9b1d8bd609785e29080130a5ff7b5b724cd2cd1
kuzan-source <h39krot@gmail.com>
2026-03-27 19:55:53 -0600

    update: configuración de barras del sistema y optimización del esquema de colores en BisiestusTheme

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Theme.kt
@@ -1,14 +1,23 @@
 package com.espiralsoft.bisiestus.presentation.ui.theme
 
+import android.app.Activity
+import android.content.Context
 import android.os.Build
+import android.view.View
+import android.view.Window
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 /**
  * Esquema de colores para el modo oscuro
@@ -66,25 +75,29 @@ private fun resolveColorScheme(
  */
 @Composable
 fun BisiestusTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
 
-    // Esquema de colores basado en el wallpaper
-    // y en el modo del dispositivo(Modo oscuro o Modo claro)
-    // pd: Este codigo fue autogenerado, asi que lo dejo nomas porque si
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val darkTheme: Boolean = isSystemInDarkTheme()
+    val context: Context = LocalContext.current
+    val view: View = LocalView.current
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme: ColorScheme = remember(darkTheme, dynamicColor)
+    {
+        resolveColorScheme(darkTheme, dynamicColor, context)
     }
 
-    //Envoltura que contiene las variables esteticas
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window: Window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).apply {
+                isAppearanceLightStatusBars = !darkTheme
+                isAppearanceLightNavigationBars = !darkTheme
+            }
+        }
+    }
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
