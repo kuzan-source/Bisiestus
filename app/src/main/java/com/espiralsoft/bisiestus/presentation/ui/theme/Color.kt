e6c0486e140116132b1401e0417aa640e91d8c12
kuzan-source <h39krot@gmail.com>
2026-04-06 13:36:17 -0600

    update: nuevas constantes de color

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/ui/theme/Color.kt
@@ -2,9 +2,10 @@ package com.espiralsoft.bisiestus.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Navy = Color(0xFF000080)
+val Indigo = Color(0xFF4B0082)
+val Plum = Color(0xFFDDA0DD)
+val RoyalBlue = Color(0xFF4169E1)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
