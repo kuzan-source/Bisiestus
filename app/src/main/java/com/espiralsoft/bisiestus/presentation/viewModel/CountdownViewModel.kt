f1edb58b9b678e6a7c74aa09942c8199f4f19b2c
kuzan-source <h39krot@gmail.com>
2026-04-08 12:30:36 -0600

    del: pequeñas correciones

--- app/src/main/java/com/espiralsoft/bisiestus/presentation/viewModel/CountdownViewModel.kt
+++ app/src/main/java/com/espiralsoft/bisiestus/presentation/viewModel/CountdownViewModel.kt
@@ -1,6 +1,5 @@
 package com.espiralsoft.bisiestus.presentation.viewModel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
